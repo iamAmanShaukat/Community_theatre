d6ff062071e2e6e3502573f69584c5390307c4bc
iamAmanShaukat <iamamanshaukat@gmail.com>
2025-02-26 15:53:15 +0000

    ImageService | handle image uploads

---
+++ src/main/java/project/community/theatre/exception/InvalidImageException.java
@@ -0,0 +1,7 @@
+package project.community.theatre.exception;
+
+public class InvalidImageException extends RuntimeException {
+    public InvalidImageException(String message) {
+        super(message);
+    }
+}
